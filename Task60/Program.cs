16297e467403d1f0bae9a6f820c053c15d45a2b0
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-18 03:19:42 +0500

    FillArray3,PrintArray

---
+++ Task60/Program.cs
@@ -0,0 +1,56 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.WriteLine("Мы будем формировать трехмерный массив из неповторяющихся натуральных чисел от 10 до 100.");
+Console.WriteLine("Введите размерность массива (3 значения).");
+Console.WriteLine("Первое значение размерности:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе значение размерности:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье значение размерности:");
+int l = Convert.ToInt32(Console.ReadLine());
+int[,,]array = new int[m,n,l];
+
+if (0<(m*n*l) && (m*n*l)<90)
+{
+    FillArrayDiff(array);
+    PrintArray(array);  
+}
+else
+{
+  Console.WriteLine("Диапазон чисел (от 10 до 100) меньше, чем размерность массива");
+}
+
+
+int[,,] FillArrayDiff(int[,,] array)
+{
+    int index = 10;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < l; k++)
+            {
+                array[i,j,k] = index;   // не сказано, что числа должны быть случайными
+                index++;
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array) 
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+           {
+                for (int k = 0; k < l; k++)
+                {
+                    Console.Write($"{array[i,j,k]} ({i},{j},{k}) ");
+                }
+                Console.WriteLine();
+            }
+        Console.WriteLine();
+    }
+}
